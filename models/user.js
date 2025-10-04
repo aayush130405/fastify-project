90832fb603c2892ea18e8a05675845ecd8b0fba6
Aayush <aayush130405@gmail.com>
2025-10-04 14:56:15 +0530

    define thumbnail model and refactor user model

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose")
 
-const userSchema = mongoose.Schema({
-    name: {type: String, require: true},
-    email: {type: String, require: true, unique: true},
-    password: {type: String, require: true},
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
     country: {type: String},
     resetPasswordToken: String,
     resetPasswordExpiry: Date,
 })
 
-module.exports = mongoose.model("user", userSchema)
+module.exports = mongoose.model("User", userSchema)
