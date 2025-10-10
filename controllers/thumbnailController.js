a039b3fc4ba234feee0ae5cd68438b85ee706df4
Aayush <aayush130405@gmail.com>
2025-10-10 19:45:03 +0530

    refactor controller

--- controllers/thumbnailController.js
+++ controllers/thumbnailController.js
@@ -9,6 +9,19 @@ const pipelineAsync = util.promisify(pipeline)
 exports.createThumbnail = async (request, reply) => {
     try {
         const parts = request.part()            //gets both file and field
+        let fields = {}
+        let filename;
+
+        for await (const part of parts) {
+            if(part.file) {
+                const filename = `${Date.now()}-${part.filename}`
+                const saveTo = path.join(
+                    __dirname, "..", "uploads", "thumbnails", filename
+                ) 
+            } else {
+                fields[part.filename] = part.value
+            } 
+        }
     } catch (err) {
         reply.send(err)
     }
